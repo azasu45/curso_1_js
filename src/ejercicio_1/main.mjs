81ca7046ce2fa20afd219ef6513b307e4afd62be
azasu <67619305+azasu45@users.noreply.github.com>
2024-01-27 15:26:26 +0000

    update 2

--- src/ejercicio_1/main.mjs
+++ src/ejercicio_1/main.mjs
@@ -4,4 +4,133 @@
 */
 import "../styles.css";
 
-console.log("Hello, World!");
+class Cl_Vehiculo {
+  constructor(placa, tipoVehiculo) {
+    this.placa = placa;
+    this.tipoVehiculo = tipoVehiculo;
+  }
+
+  Pago() {
+    if (this.tipoVehiculo === 1) {
+      return 10;
+    } else if (this.tipoVehiculo === 2) {
+      return 20;
+    } else if (this.tipoVehiculo === 3) {
+      return 50;
+    }
+  }
+}
+
+class Cl_Peaje {
+  constructor() {
+    this.acumPagosTotal = 0;
+    this.contTipo1 = 0;
+    this.contTipo2 = 0;
+    this.contTipo3 = 0;
+    this.contTotal = 0;
+  }
+  ProcesarVehiculo(vehiculo) {
+    if (vehiculo.tipoVehiculo === 1) {
+      this.contTipo1++;
+    } else if (vehiculo.tipoVehiculo === 2) {
+      this.contTipo2++;
+    } else if (vehiculo.tipoVehiculo === 3) {
+      this.contTipo3++;
+    }
+    this.contTotal++;
+    this.acumPagosTotal += vehiculo.Pago();
+  }
+
+  PorcentajeVehiculoTipo1() {
+    return (this.contTipo1 / this.contTotal) * 100;
+  }
+
+  PorcentajeVehiculoTipo2() {
+    return (this.contTipo2 / this.contTotal) * 100;
+  }
+
+  PorcentajeVehiculoTipo3() {
+    return (this.contTipo3 / this.contTotal) * 100;
+  }
+
+  TotalPagos() {
+    return this.acumPagosTotal * 0.4;
+  }
+}
+
+class App {
+  constructor() {
+    this.salidaCabecera = document.getElementById("salida_cabecera");
+    this.salidaRegistros = document.getElementById("salida_registros");
+    this.salidaPie = document.getElementById("salida_pie");
+    this.salidaRespuestas = document.getElementById("salida_respuestas");
+  }
+
+  TableHead() {
+    return `
+    <tr>
+      <th>Placa</th>
+      <th>Tipo de Vehiculo</th>
+      <th>Pago( )</th>
+    </tr>
+    `;
+  }
+
+  TableRow(placa, tipoVehiculo, pago) {
+    return `
+    <tr>
+      <td>${placa}</td>
+      <td>${tipoVehiculo}</td>
+      <td>${pago}</td>
+    </tr>
+    `;
+  }
+
+  TableFoot(totalPagos) {
+    return `
+    <tr>
+      <th colspan="2">Total pagos</th>
+      <th>${totalPagos}</th>
+    </tr>
+    `;
+  }
+
+  run() {
+    let peaje = new Cl_Peaje();
+
+    let vehiculo1 = new Cl_Vehiculo("111", 1);
+    let vehiculo2 = new Cl_Vehiculo("222", 3);
+    let vehiculo3 = new Cl_Vehiculo("333", 2);
+    let vehiculo4 = new Cl_Vehiculo("444", 1);
+
+    peaje.ProcesarVehiculo(vehiculo1);
+    peaje.ProcesarVehiculo(vehiculo2);
+    peaje.ProcesarVehiculo(vehiculo3);
+    peaje.ProcesarVehiculo(vehiculo4);
+
+    this.salidaCabecera.innerHTML = `
+      ${this.TableHead()}
+    `;
+
+    this.salidaRegistros.innerHTML = `
+      ${this.TableRow(vehiculo1.placa, vehiculo1.tipoVehiculo, vehiculo1.Pago())}
+      ${this.TableRow(vehiculo2.placa, vehiculo2.tipoVehiculo, vehiculo2.Pago())}
+      ${this.TableRow(vehiculo3.placa, vehiculo3.tipoVehiculo, vehiculo3.Pago())}
+      ${this.TableRow(vehiculo4.placa, vehiculo4.tipoVehiculo, vehiculo4.Pago())}
+    `;
+
+    this.salidaPie.innerHTML = `
+      ${this.TableFoot(peaje.acumPagosTotal)}
+    `;
+
+    this.salidaRespuestas.innerHTML = `
+    <h3>a) Pocentaje por vehiculo </h3>
+    <p>Tipo 1 = ${peaje.PorcentajeVehiculoTipo1()} %</p>
+    <p>Tipo 2 = ${peaje.PorcentajeVehiculoTipo2()} %</p>
+    <p>Tipo 3 = ${peaje.PorcentajeVehiculoTipo3()} %</p>
+    <h3>b) Al peaje le toca el 40% son ${peaje.TotalPagos()}}</h3>
+  `;
+  }
+}
+
+new App().run();
--- src/ejercicio_3/main.mjs
+++ src/ejercicio_3/main.mjs
@@ -1,7 +1,91 @@
 /*
     El tipo de archivo de este archivo es: .mjs
-    para importar
+    para importar el style
 */
 import "../styles.css";
 
-console.log("Hello, World!");
+class Cl_Menor {
+  constructor(a) {
+    this.atributo = a;
+  }
+
+  Metodo(parametro) {
+    return parametro;
+  }
+}
+
+class Cl_Mayor {
+  constructor() {
+    this.contadorTotalClaseMenor = 0;
+    this.acumuladorDeCantidadDeLetras = 0;
+  }
+
+  ProcesarMenor(menor) {
+    this.acumuladorDeCantidadDeLetras += menor.atributo.length;
+    this.contadorTotalClaseMenor++;
+  }
+}
+
+class App {
+  constructor() {
+    this.salidaCabecera = document.getElementById("salida_cabecera");
+    this.salidaRegistros = document.getElementById("salida_registros");
+    this.salidaPie = document.getElementById("salida_pie");
+    this.salidaRespuestas = document.getElementById("salida_respuestas");
+  }
+
+  TableHead() {
+    return `
+    <tr>
+      <th>atributo</th>
+      <th>Metodo( )</th>
+    </tr>
+    `;
+  }
+
+  TableRow(atributo, metodo) {
+    return `
+    <tr>
+      <td>${atributo}</td>
+      <td>${metodo}</td>
+    </tr>
+    `;
+  }
+
+  TableFoot(totalPagos) {
+    return `
+    <tr>
+      <th colspan="1">Total letras de los nombre</th>
+      <th>${totalPagos}</th>
+    </tr>
+    `;
+  }
+
+  run() {
+    let mayor = new Cl_Mayor();
+    let menor1 = new Cl_Menor("Nombre", 1);
+    let menor2 = new Cl_Menor("Nombre2", 1);
+
+    mayor.ProcesarMenor(menor1);
+    mayor.ProcesarMenor(menor2);
+
+    this.salidaCabecera.innerHTML = `
+      ${this.TableHead()}
+    `;
+
+    this.salidaRegistros.innerHTML = `
+      ${this.TableRow(menor1.atributo, menor1.Metodo(":D"))}
+      ${this.TableRow(menor2.atributo, menor2.Metodo(":("))}
+    `;
+
+    this.salidaPie.innerHTML = `
+      ${this.TableFoot(mayor.acumuladorDeCantidadDeLetras)}
+    `;
+
+    this.salidaRespuestas.innerHTML = `
+      <h3>a) total de registros: ${mayor.contadorTotalClaseMenor} </h3>
+    `;
+  }
+}
+
+new App().run();
